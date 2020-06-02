6a1d49c25f63eab26cb2dd13545025fecb285d14
Moizuddin Qureshi <30798716+imoizuddin@users.noreply.github.com>
2020-06-02 13:20:56 +0530

    Pre Final Changes

--- SentenceSplitter.py
+++ SentenceSplitter.py
@@ -1,9 +1,6 @@
 from indicnlp.tokenize import sentence_tokenize
 
 
-#
-# class ClassSplitter:
-
 def code_splitter(string):
     sentences = sentence_tokenize.sentence_split(string, lang='hi')
     for t in sentences:
--- p_gui.py
+++ p_gui.py
@@ -9,7 +9,7 @@ def show_answer():
     # inputString = hindi_string.get()
     inputString = self.MyEntryBox.get("1.0", END)
     stemDict = MainCode.mainfunctioncode(inputString)
-    output.insert('1.0', stemDict)
+    output2.insert('1.0', stemDict)
 
 
 main = Tk()
@@ -18,26 +18,28 @@ main.configure(background="#dddddd")
 main.geometry("1920x1080")
 Label(main, text="Enter \nSentence:", bg="#dddddd",
       fg="black", font="none 12 bold").place(x=15, y=10)
-Label(main, text="Output:", bg="#dddddd",
+Label(main, text="Tokens:", bg="#dddddd",
       fg="black", font="none 12 bold").place(x=15, y=220)
-
+Label(main, text="Root \nWords:", bg="#dddddd",
+      fg="black", font="none 12 bold").place(x=15, y=350)
+Label(main, text="POS Tags:", bg="#dddddd",
+      fg="black", font="none 12 bold").place(x=15, y=510)
 
 self.MyEntryBox = Text(main, height=3, width=170, relief=RIDGE)
 self.MyEntryBox.place(x=120, y=15)
-# hindi_string = Entry(main)
-# blank = Entry(main)
 
-output = Text(main, width=170, height=25, wrap=WORD, background="white")
-output.place(x=120, y=220)
+output = Text(main, width=170, height=9, wrap=WORD, background="white")
+output.place(x=120, y=170)
 
-# hindi_string.grid(row=4, column=5)
-# blank.grid(row=15, column=37)
+output1 = Text(main, width=170, height=9, wrap=WORD, background="white")
+output1.place(x=120, y=330)
 
+output2 = Text(main, width=170, height=9, wrap=WORD, background="white")
+output2.place(x=120, y=490)
 
 Button(main, text='Quit', width=25, height=3, command=main.destroy).place(
     x=1100, y=700)
 Button(main, text='Show', width=25, height=3, command=show_answer).place(
     x=700, y=75)
-
 
 mainloop()
